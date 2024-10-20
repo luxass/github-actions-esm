928ca03b8fbc39bb4246f3304bca5024bab06c4d
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-20 16:54:39 +0200

    debug

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import { readFile } from "node:fs/promises"
+
 async function run() {
   console.log("Hello, World!");
+
+  const data = await readFile("README.md", "utf-8");
+
+  console.log(data);
 }
 
 run().catch((err) => {
